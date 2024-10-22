d36280eaa3f3a9bde71d477b4d5c6cf1e8f9b940
stablelane <aaanshad09@gmail.com>
2024-10-23 00:36:00 +0530

    Add function to open browser with movie link based on OS

--- main.go
+++ main.go
@@ -1,27 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
+
 	// "reflect"
-	"encoding/json"
+
 	"github.com/manifoldco/promptui"
 )
 
 type Movie struct {
-    Title  string `json:"Title"`
-    Year   string `json:"Year"`
-    ImdbID string `json:"imdbID"`
-    Type   string `json:"Type"`
-    Poster string `json:"Poster"`
+	Title  string `json:"Title"`
+	Year   string `json:"Year"`
+	ImdbID string `json:"imdbID"`
+	Type   string `json:"Type"`
+	Poster string `json:"Poster"`
 }
 
 type Response struct {
-    Search       []Movie `json:"Search"`
-    TotalResults string   `json:"totalResults"`
-    Response     string   `json:"Response"`
+	Search       []Movie `json:"Search"`
+	TotalResults string  `json:"totalResults"`
+	Response     string  `json:"Response"`
 }
 
 func main() {
@@ -29,17 +33,18 @@ func main() {
 		fmt.Println("Usage: your-cli-tool <command>")
 		return
 	} else {
-		x  := os.Args[1]
+		x := os.Args[1]
 		apiKey := "&apikey=1170f02c"
 		baseURL := "http://www.omdbapi.com/?s="
 		apiLink := baseURL + x + apiKey
 		fmt.Println(apiLink)
-		res , err := http.Get(apiLink)
+		//fetching data from omdbapi using movie name from user 
+		res, err := http.Get(apiLink)
 		if err != nil {
 			fmt.Println(err)
 		}
 		defer res.Body.Close()
-		body , err := io.ReadAll(res.Body)
+		body, err := io.ReadAll(res.Body)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -50,39 +55,50 @@ func main() {
 			return
 		}
 
-		//create an array to store the movies from seach 
+		//create an array to store the movies from seach
 		var movies []string
-
-		
 
 		// Access each movie in the Search array
 		for _, movie := range response.Search {
-			movies = append(movies,fmt.Sprintf("Title: %s, Year: %s, IMDb ID: %s, Type: %s", movie.Title, movie.Year, movie.ImdbID, movie.Type))
+			movies = append(movies, fmt.Sprintf("Title: %s, Year: %s, IMDb ID: %s, Type: %s", movie.Title, movie.Year, movie.ImdbID, movie.Type))
 		}
 		// fmt.Println(movies)
 
-		//create 
+		//create a list in cli for users to select movies
 		prompt := promptui.Select{
 			Label: "Select Movie",
 			Items: movies,
 		}
-		_, result, err := prompt.Run()
+		index, _, err := prompt.Run()
 		if err != nil {
 			fmt.Printf("Promt failed %v\n", err)
 			return
 		}
+		var selectedMovie = response.Search[index]
+		var selectedMovieId = selectedMovie.ImdbID
+		openLink(selectedMovieId)
+	}
 
-		fmt.Printf("You choose %q\n", result)
-		}
-	
 }
-// for i:=0; i<len(os.Args); i++ {
-// 	fmt.Println(os.Args[i])
-// } 
-// switch os.Args[1] {
-// case "greet":
-// 	fmt.Println("Hello!")
-// default:
-// 	fmt.Println("Unknown command:", os.Args[1])
-	
-// }
+
+// function to check the OS of evironment and open browser with the link using the imdbId as query
+func openLink(id string) error {
+	var baseURL = "https://multiembed.mov/?video_id="
+	var url = baseURL + id
+
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "linux":
+		cmd = exec.Command("xdg-open", url)
+	case "darwin":
+		cmd = exec.Command("open", url)
+	case "windows":
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+	default:
+		return fmt.Errorf("unsupported platform")
+
+	}
+	return cmd.Start()
+
+}
