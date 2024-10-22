777bc0c3d76763693786255d050999e1f0d291e0
stablelane <aaanshad09@gmail.com>
2024-10-22 21:34:18 +0530

    add promptui package and its function

--- main.go
+++ main.go
@@ -6,7 +6,23 @@ import (
 	"net/http"
 	"os"
 	// "reflect"
+	"encoding/json"
+	"github.com/manifoldco/promptui"
 )
+
+type Movie struct {
+    Title  string `json:"Title"`
+    Year   string `json:"Year"`
+    ImdbID string `json:"imdbID"`
+    Type   string `json:"Type"`
+    Poster string `json:"Poster"`
+}
+
+type Response struct {
+    Search       []Movie `json:"Search"`
+    TotalResults string   `json:"totalResults"`
+    Response     string   `json:"Response"`
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -27,8 +43,37 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println(string(body))
-	}
+		var response Response
+		err = json.Unmarshal(body, &response)
+		if err != nil {
+			fmt.Println("Error unmarshaling JSON:", err)
+			return
+		}
+
+		//create an array to store the movies from seach 
+		var movies []string
+
+		
+
+		// Access each movie in the Search array
+		for _, movie := range response.Search {
+			movies = append(movies,fmt.Sprintf("Title: %s, Year: %s, IMDb ID: %s, Type: %s", movie.Title, movie.Year, movie.ImdbID, movie.Type))
+		}
+		// fmt.Println(movies)
+
+		//create 
+		prompt := promptui.Select{
+			Label: "Select Movie",
+			Items: movies,
+		}
+		_, result, err := prompt.Run()
+		if err != nil {
+			fmt.Printf("Promt failed %v\n", err)
+			return
+		}
+
+		fmt.Printf("You choose %q\n", result)
+		}
 	
 }
 // for i:=0; i<len(os.Args); i++ {
